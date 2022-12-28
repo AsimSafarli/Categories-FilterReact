704037e9cc534ae66073ca751da0ca192fc0eab1
AsimSafarli <asimseferli014@gmail.com>
2022-12-28 21:26:20 +0400

    initial commit

---
+++ src/Data.js
@@ -0,0 +1,83 @@
+const menu = [
+    {
+      id: 1,
+      title: 'Ciyelikli Pankek',
+      category: 'breakfast',
+      price: 5,
+      img:
+        'https://img.freepik.com/free-photo/tartlets-with-pistachios-strawberry-cream-chocolate-side-view_141793-4156.jpg?w=740&t=st=1670785483~exp=1670786083~hmac=0be16a8b382ea8f200711d517cc3d8a4fbd4c6fd21c984114a35b1d0320edbab',
+      desc: `lorem lorem lorem lorem lorem lorem lorem lorem lorem lokre  .`,
+    },
+    {
+      id: 2,
+      title: 'Pizza',
+      category: 'lunch',
+      price: 25,
+      img:
+        'https://img.freepik.com/free-photo/margarita-pizza-with-tomato-side-view_141793-2769.jpg?w=740&t=st=1670782051~exp=1670782651~hmac=b19990bd9849e3d7329e997a02554fa7124fe7c7aaf52a60431b56e42d5f3882',
+      desc: `Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lokre `,
+    },
+    {
+      id: 3,
+      title: 'Milli yemek',
+      category: 'dinner',
+      price: 50,
+      img: 'https://img.freepik.com/premium-photo/traditional-turkish-cuisine_127425-87.jpg?w=740',
+      desc: `Photo traditional turkish cuisine.Photo traditional turkish cuisine.Photo traditional turkish cuisine.`,
+    },
+    {
+      id: 4,
+      title: 'Breakfast',
+      category: 'breakfast',
+      price: 15,
+      img:
+        'https://img.freepik.com/free-photo/turkish-breakfast-platter-with-cheese-vegetables-olives-jams-sausages-flatbread-wrap_140725-6860.jpg?w=360&t=st=1670782219~exp=1670782819~hmac=059b997b87a9f2663965e93ec3d952d07cfdb22ced3d1aecc8db81d475e33005',
+      desc: `Breakfast lorem lorem Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lokre  `,
+    },
+    {
+      id: 5,
+      title: 'Kebab',
+      category: 'lunch',
+      price: 35,
+      img:
+        'https://img.freepik.com/free-photo/kebab-platter-with-tikka-lula-chicken-vegetable-kebabs_140725-256.jpg?w=740&t=st=1670787366~exp=1670787966~hmac=5da7d846dae4d058f6f891fc3656a33b53f8be9e0b0fe020c9db933a6ae377d8',
+      desc: `photo kebab platter with tikka, lula, chicken and vegetable kebab photo kebab platter with tikka, lula, chicken and vegetable kebab `,
+    },
+    {
+      id: 6,
+      title: 'Super Dest',
+      category: 'dinner',
+      price: 150,
+      img:
+        'https://img.freepik.com/premium-photo/thanksgiving-celebration_53876-73751.jpg?w=740',
+      desc: `10 neferlik destimiz sizin ucun onemli hazrlanib`,
+    },
+    {
+      id: 7,
+      title: 'Qaragat',
+      category: 'breakfast',
+      price: 50,
+      img:
+        'https://img.freepik.com/premium-photo/delicious-homemade-pancakes-with-strawberries-blueberries-honey-dark-wooden-background-with-blue-napkin-concept-healthy-nutritious-breakfast-rustic-style-macro-close-up_185452-1918.jpg?w=360',
+      desc: `Breakfast lorem lorem Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lokre  `,
+    },
+    {
+      id: 8,
+      title: 'Skeyit',
+      category: 'lunch',
+      price: 30,
+      img:
+        'https://img.freepik.com/free-photo/meat-kebab-skew-with-vegetables_140725-2151.jpg?t=st=1670787366~exp=1670787966~hmac=4533fc015f947ce78eccb8f020897eb89b8e9479db052c25ee4839fcb3076eb9',
+      desc: `Meat kebab on a skew with vegetablesMeat kebab on a skew with vegetablesMeat kebab on a skew with vegetables `,
+    },
+    {
+      id: 9,
+      title: 'Thanksgiving Seti',
+      category: 'dinner',
+      price: 100,
+      img:
+        'https://img.freepik.com/free-photo/top-view-delicious-thanksgiving-meal_23-2148629564.jpg?w=826&t=st=1670787897~exp=1670788497~hmac=8c2ba0e7c6309c006945b46acc498fc641a917a0b8bfaad112c0e29da238f2ad',
+      desc: `Top view of delicious thanksgiving mealTop view of delicious thanksgiving mealTop view of delicious thanksgiving mealTop view of delicious thanksgiving meal`,
+    },
+  ]
+  export default menu
